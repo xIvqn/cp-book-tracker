a81fee4cdd21522da2c898d70f5c756bda3c4d45
xIvqn <xivqn@proton.me>
2023-04-04 13:50:25 +0200

    feat: add bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ProblemComponent } from './components/problem/problem.component';
@@ -18,6 +19,7 @@ import { ProblemSetComponent } from './components/problem-set/problem-set.compon
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     HttpClientModule
   ],
   providers: [],
