44aa55969f4da07bd54bc2407b31294d47ae0db6
Iván <72264031+xIvqn@users.noreply.github.com>
2025-12-23 19:52:54 +0100

    test: create unit tests

--- cp-book-tracker/src/app/services/user.service.spec.ts
+++ cp-book-tracker/src/app/services/user.service.spec.ts
@@ -1,16 +1,82 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return solved problem IDs when user exists', () => {
+    const dummyUsername = 'testuser';
+    const dummyUid = 123;
+    const dummySolvedData = [
+      {
+        solved: [3, 0] // Problems 0 and 1 are solved
+      }
+    ];
+
+    service.getSolved(dummyUsername).subscribe((solved) => {
+      expect(solved).toEqual([0, 1]);
+    });
+
+    const reqUid = httpMock.expectOne(`${service.url}/uname2uid/${dummyUsername}`);
+    expect(reqUid.request.method).toBe('GET');
+    reqUid.flush(dummyUid);
+
+    const reqSolved = httpMock.expectOne((req) => req.url.startsWith(`${service.url}/solved-bits/${dummyUid}`));
+    expect(reqSolved.request.method).toBe('GET');
+    reqSolved.flush(dummySolvedData);
+  });
+
+  it('should handle multiple blocks of solved bits', () => {
+    const dummyUsername = 'testuser';
+    const dummyUid = 123;
+    const dummySolvedData = [
+      {
+        solved: [1, 1] // Bits: 0 in first 32-bit int, 0 in second 32-bit int.
+        // Problem 0 and Problem 32.
+      }
+    ];
+
+    service.getSolved(dummyUsername).subscribe((solved) => {
+      expect(solved).toEqual([0, 32]);
+    });
+
+    const reqUid = httpMock.expectOne(`${service.url}/uname2uid/${dummyUsername}`);
+    reqUid.flush(dummyUid);
+
+    const reqSolved = httpMock.expectOne((req) => req.url.startsWith(`${service.url}/solved-bits/${dummyUid}`));
+    reqSolved.flush(dummySolvedData);
+  });
+
+  it('should return empty list if user does not exist (id = 0)', () => {
+    const dummyUsername = 'nonexistent';
+
+    service.getSolved(dummyUsername).subscribe((solved) => {
+      expect(solved).toEqual([]);
+    });
+
+    const reqUid = httpMock.expectOne(`${service.url}/uname2uid/${dummyUsername}`);
+    reqUid.flush(0);
+
+    const reqSolved = httpMock.expectOne((req) => req.url.startsWith(`${service.url}/solved-bits/0`));
+    reqSolved.flush([]);
   });
 });
